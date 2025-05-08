3ea39acd9917f26dc1ff5c7c963cc9aaa2a4a66d
Andrew <akolomiec982@gmail.com>
2025-05-09 00:17:27 +0300

    Implemented viewsets for Train, Station, Route, Crew, Journey, and Order with filtering and image upload

--- railway_station/views.py
+++ railway_station/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from drf_spectacular.types import OpenApiTypes
 from rest_framework import viewsets, mixins, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser
@@ -5,9 +8,11 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 
-from railway_station.models import Train, TrainType
+from railway_station.models import Train, TrainType, Station, Route, Crew, Journey, Order
 from railway_station.serializers import TrainTypeSerializer, TrainSerializer, TrainListSerializer, \
-    TrainRetrieveSerializer, TrainImageSerializer
+    TrainRetrieveSerializer, TrainImageSerializer, StationSerializer, RouteSerializer, RouteListSerializer, \
+    RouteRetrieveSerializer, CrewSerializer, JourneyListSerializer, JourneyRetrieveSerializer, JourneySerializer, \
+    OrderSerializer
 
 
 class TrainTypeViewSet(viewsets.ModelViewSet):
@@ -50,6 +55,7 @@ class TrainViewSet(
             queryset = queryset.filter(
                 train_type__id__in=train_type
             )
+
         if self.action in ("list", "retrieve"):
             queryset = queryset.select_related("train_type")
         return queryset.distinct()
@@ -72,7 +78,7 @@ class TrainViewSet(
     @extend_schema(
         parameters=[
             OpenApiParameter(
-                name="train_type",
+                "train_type",
                 type={"type": "array", "items": {"type": "number"}},
                 description="Filter by train_type id (ex. ?train_type=2,3)",
             ),
@@ -81,3 +87,130 @@ class TrainViewSet(
     def list(self, request, *args, **kwargs):
         """Get list of all trains."""
         return super().list(request, *args, **kwargs)
+
+
+class StationViewSet(viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all().select_related("source", "destination")
+    serializer_class = RouteSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+        elif self.action == "retrieve":
+            return RouteRetrieveSerializer
+
+        return RouteSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        source_param = self.request.query_params.get("source")
+        destination_param = self.request.query_params.get("destination")
+
+        if source_param:
+            queryset = queryset.filter(source=source_param)
+
+        if destination_param:
+            queryset = queryset.filter(destination=destination_param)
+
+        return  queryset
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class JourneyViewSet(viewsets.ModelViewSet):
+    queryset = Journey.objects.all().select_related(
+        "train",
+        "route",
+        "route__source",
+        "route__destination"
+    ).prefetch_related(
+        "crew"
+    )
+    serializer_class = JourneySerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return JourneyListSerializer
+        elif self.action == "retrieve":
+            return JourneyRetrieveSerializer
+
+        return JourneySerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        start_data = self.request.query_params.get("start")
+        route_id_str = self.request.query_params.get("route")
+
+        if start_data:
+            start_data = datetime.strptime(start_data, "%Y-%m-%d").date()
+            queryset = queryset.filter(departure_time___date=start_data)
+
+        if route_id_str:
+            queryset = queryset.filter(route_id=int(route_id_str))
+
+        return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="start",
+                type=OpenApiTypes.DATE,
+                description=
+                "Filter journey (trips) starting from the specified date (YYYY-MM-DD)"
+                " (ex. ?start=2025-05-08)",
+                location=OpenApiParameter.QUERY,
+                required=False,
+            ),
+            OpenApiParameter(
+                name="route",
+                type=OpenApiTypes.INT,
+                description="Filter journey (trips) by route ID (ex. ?route=101)",
+                location=OpenApiParameter.QUERY,
+                required=False,
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        """
+        Get a list of all journey (trips) with the ability to filter by start date and route ID.
+        """
+        return super().list(request, *args, **kwargs)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="id",
+                type=OpenApiTypes.INT,
+                description="Ідентифікатор подорожі.",
+                location=OpenApiParameter.PATH,
+                required=True,
+            ),
+        ]
+    )
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Get detailed information about a specific journey (trip) by its ID.
+        """
+        return super().retrieve(request, *args, **kwargs)
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all().select_related("user")
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user).prefetch_related("tickets").select_related("user")
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
