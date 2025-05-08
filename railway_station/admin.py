9a5afd81871ce182d354a13cbd66177f29d5bdd0
Andrew <akolomiec982@gmail.com>
2025-05-08 23:56:08 +0300

    Register Train, TrainType, Station, Route, Crew, Ticket, Order models in admin panel

--- railway_station/admin.py
+++ railway_station/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Train, TrainType
+from .models import Train, TrainType, Station, Route, Crew, Ticket, Order
 
 admin.site.register(Train)
 admin.site.register(TrainType)
+admin.site.register(Station)
+admin.site.register(Route)
+admin.site.register(Crew)
+admin.site.register(Ticket)
+admin.site.register(Order)
+
